0c9ab27a47d155fdf89a4cbcff9bffef5d868553
Thanh Dat <datmathoingu@gmail.com>
2020-09-22 05:44:20 +0700

    Initial commit

---
+++ Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessModifier
+{
+    public class Circle
+    {
+        private double Radius = 1.0;
+        private string Color = "Red";
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+        }
+        public Circle()
+        {
+
+        }
+        public void setRadius(double radius)
+        {
+            Radius = radius;
+        }
+        public double getRadius()
+        {
+            return Radius;
+        }
+        public void setColor(string color)
+        {
+            Color = color;
+        }
+        public string getColor()
+        {
+            return Color;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AccessModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            circle.setColor("Red");
+            circle.setRadius(2);
+            Console.WriteLine(circle.getColor());
+            Console.WriteLine(circle.getRadius());
+        }
+    }
+}
